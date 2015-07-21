8b0fe71d118d2bdca5ca51dd95eaf0fadc8230c8
mplacona <marcos.placona@gmail.com>
2015-07-21 11:55:35 +0100

    Add comments to code

--- TwilioMakeAndReceiveCalls/Controllers/HomeController.cs
+++ TwilioMakeAndReceiveCalls/Controllers/HomeController.cs
@@ -18,12 +18,14 @@ namespace TwilioMakeAndReceiveCalls.Controllers
         public ActionResult MakeCall(string id)
         {
             
+            // Instantiate new Rest API object
             var client = new TwilioRestClient(Settings.AccountSid, Settings.AuthToken);
             client.InitiateOutboundCall(Settings.MyNumber, string.Format("+{0}", id), "http://www.televisiontunes.com/uploads/audio/Star%20Wars%20-%20The%20Imperial%20March.mp3");
 
             return Content("The call has been is initiated");
         }
 
+        // Get: ReceiveCall
         public ActionResult ReceiveCall()
         {
             var twiml = new TwilioResponse();
