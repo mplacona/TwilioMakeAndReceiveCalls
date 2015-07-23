decd6104540ef78283c5da48f1cd3995d4dc7ff6
mplacona <marcos.placona@gmail.com>
2015-07-23 17:15:21 +0100

    Add more settings and error checking

--- TwilioMakeAndReceiveCalls/Controllers/HomeController.cs
+++ TwilioMakeAndReceiveCalls/Controllers/HomeController.cs
@@ -20,16 +20,22 @@ namespace TwilioMakeAndReceiveCalls.Controllers
             
             // Instantiate new Rest API object
             var client = new TwilioRestClient(Settings.AccountSid, Settings.AuthToken);
-            client.InitiateOutboundCall(Settings.MyNumber, string.Format("+{0}", id), "http://www.televisiontunes.com/uploads/audio/Star%20Wars%20-%20The%20Imperial%20March.mp3");
+            var result = client.InitiateOutboundCall(Settings.MyTwilioNumber, string.Format("+{0}", id), "http://www.televisiontunes.com/uploads/audio/Star%20Wars%20-%20The%20Imperial%20March.mp3");
+
+            // return any exceptions that may occur
+            if (result.RestException != null)
+            {
+                return Content(result.RestException.Message);
+            }
 
             return Content("The call has been is initiated");
         }
-
+            
         // Get: ReceiveCall
         public ActionResult ReceiveCall()
         {
             var twiml = new TwilioResponse();
-            return TwiML(twiml.Say("You are calling Marcos Placona").Dial(Settings.MyNumber));
+            return TwiML(twiml.Say("You are calling Marcos Placona").Dial(Settings.MyOwnNumber));
         }
     }
 }
--- TwilioMakeAndReceiveCalls/Controllers/Settings.cs
+++ TwilioMakeAndReceiveCalls/Controllers/Settings.cs
@@ -6,6 +6,7 @@ namespace TwilioMakeAndReceiveCalls.Controllers
     {
         public static string AccountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
         public static string AuthToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
-        public static string MyNumber = Environment.GetEnvironmentVariable("MY_NUMBER");
+        public static string MyOwnNumber = Environment.GetEnvironmentVariable("MY_NUMBER");
+        public static string MyTwilioNumber = Environment.GetEnvironmentVariable("TWILIO_NUMBER");
     }
 }
